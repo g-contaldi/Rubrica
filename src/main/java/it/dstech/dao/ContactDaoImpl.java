06c60d81331263040b2dba418f773f5fc817332c
gianluca <g.contaldi@dstech.it>
2017-10-12 13:10:46 +0200

    added hibernate criteria

--- src/main/java/it/dstech/dao/ContactDaoImpl.java
+++ src/main/java/it/dstech/dao/ContactDaoImpl.java
@@ -2,7 +2,8 @@ package it.dstech.dao;
 
 import java.util.ArrayList;
 
-import org.hibernate.Query;
+import org.hibernate.Criteria;
+import org.hibernate.criterion.Restrictions;
 
 import it.dstech.model.Contact;
 import it.dstech.model.User;
@@ -14,19 +15,36 @@ public class ContactDaoImpl extends HibernateDao implements ContactDao {
 		persist(contact);
 	}
 
+	// @Override
+	// public Contact getContactById(int id) {
+	// Query query = select("SELECT c FROM Contact c WHERE c.id=:id");
+	// query.setParameter("id", id);
+	// return (Contact) query.uniqueResult();
+	// }
+
 	@Override
 	public Contact getContactById(int id) {
-		Query query = select("SELECT c FROM Contact c WHERE c.id=:id");
-		query.setParameter("id", id);
-		return (Contact) query.uniqueResult();
+		Criteria cr = getSession().createCriteria(Contact.class);
+		cr.add(Restrictions.eq("id", id));
+		return (Contact) cr.uniqueResult();
 	}
+
+	// @SuppressWarnings("unchecked")
+	// @Override
+	// public ArrayList<Contact> getListContactsByUser(User user) {
+	// Query query = select("SELECT c FROM Contact c JOIN c.user u WHERE
+	// u.id=:id");
+	// query.setParameter("id", user.getId());
+	// return (ArrayList<Contact>) query.list();
+	// }
 
 	@SuppressWarnings("unchecked")
 	@Override
 	public ArrayList<Contact> getListContactsByUser(User user) {
-		Query query = select("SELECT c FROM Contact c JOIN c.user u WHERE u.id=:id");
-		query.setParameter("id", user.getId());
-		return (ArrayList<Contact>) query.list();
+		Criteria cr = getSession().createCriteria(Contact.class, "contact");
+		cr.createAlias("contact.user", "user");
+		cr.add(Restrictions.eq("user.id", user.getId()));
+		return (ArrayList<Contact>) cr.list();
 	}
 
 	@Override
